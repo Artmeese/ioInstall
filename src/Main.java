ab2c5970c6c52bd20ab679272e93cc22e913775a
artsvvy <artsvvy@gmail.com>
2022-06-13 21:30:01 +0500

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,10 @@ public class Main {
                 new File("E:/Idea//Netology//JavaCore//Games//savegames"),
                 new File("E:/Idea//Netology//JavaCore//Games//temp"),
                 new File("E:/Idea//Netology//JavaCore//Games//src//main"),
-                new File("E:/Idea//Netology//JavaCore//Games//src//test")
+                new File("E:/Idea//Netology//JavaCore//Games//src//test"),
+                new File("E:/Idea//Netology//JavaCore//Games//res//drawables"),
+                new File("E:/Idea//Netology//JavaCore//Games//res//vectors"),
+                new File("E:/Idea//Netology//JavaCore//Games//res//icons")
         };
 
         StringBuilder log = new StringBuilder();
@@ -28,7 +31,8 @@ public class Main {
 
         File[] file = {
                 new File(dir[4], "Main.java"),
-                new File(dir[4], "Utils.java")
+                new File(dir[4], "Utils.java"),
+                new File(dir[3], "temp.txt")
         };
 
         for(int i = 0; i < file.length; i++) {
@@ -43,44 +47,19 @@ public class Main {
             }
         }
 
-        File[] dir2 = {
-                new File(dir[1].getAbsolutePath() + "//drawables"),
-                new File(dir[1].getAbsolutePath() + "//vectors"),
-                new File(dir[1].getAbsolutePath() + "//icons"),
-        };
-
-        for(int i = 0; i < dir2.length; i++) {
-            if (dir2[i].mkdir()) {
-                log.append("Catalog " + dir2[i].getName() + "has been created\n");
-            } else {
-                log.append("Catalog " + dir2[i].getName() + "was not created\n");
-            }
-        }
-
-        File file2 = new File(dir[3], "temp.txt");
-
-        try {
-            if (file2.createNewFile()) {
-                log.append("File " + file2.getName() + "has been created\n");
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-
-        if (file2.canWrite()) {
-            log.append("The file " + file2.getName() + " can be written\n");
+        if (file[2].canWrite()) {
+            log.append("The file " + file[2].getName() + " can be written\n");
         } else {
-            log.append("The file " + file2.getName() + " can't be written\n");
+            log.append("The file " + file[2].getName() + " can't be written\n");
         }
         String logText = log.toString();
 
-        try (FileWriter writer = new FileWriter(file2.getAbsolutePath())){
+        try (FileWriter writer = new FileWriter(file[2].getAbsolutePath())){
             writer.write(logText);
             writer.flush();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
         System.out.println("Directories and catalogs has been created. See temp.txt");
     }
 }
